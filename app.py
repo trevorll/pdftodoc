e1f9324c93d520a77a5a6b33832f11e81abbe04e
godwill <godwillkisia5@gmail.com>
2023-10-16 12:33:46 +0300

    added image bacground remover

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from pdf2docx import Converter
 from rembg import remove
 import numpy as np
 from PIL import Image
+from docx import Document
+
+import PyPDF2
+from docx import Document
 
 
 
@@ -14,8 +18,32 @@ app = Flask(__name__)
 def home():
     pdf = '/home/godwill/sd/receipt.pdf'
     docx = '/home/godwill/Documents/output1.docx'
+    direct = '/home/godwill/Documents/receipt'
     cv = Converter(pdf)
     cv.convert(docx,start=0, end=None)
+    
+
+    # Open the PDF file
+    pdf_file = open(pdf, 'rb')
+
+    # Create a PDF reader object
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    # Create a new Word document
+    doc = Document()
+
+    # Iterate through each page of the PDF
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text = page.extract_text()  # Extract text from the PDF page
+        doc.add_paragraph(text)    # Add the text to the Word document
+
+    # Save the Word document
+    doc.save('/home/godwill/Documents/out.docx')
+
+    # Close the PDF file
+    pdf_file.close()
+
     cv.close()
     print('successfull')
     return('hello')
@@ -31,4 +59,4 @@ def rmbg():
     return 'success'
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
